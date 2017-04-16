8e0053aad3be99dd9e25b53a9694bf20f1857912
SScript <sscriptnet@gmail.com>
2017-04-16 18:35:51 +0100

    udpate

--- app.js
+++ app.js
@@ -1,50 +1,37 @@
     var express = require('express')
     var app = express()
     var port = Number(process.env.PORT||8080);
+var http = require('http')
+
+    var dns = require('dns')
+    var os = require('os')
+
+    var obj = {};
 
     app.get('/', function(req, res) {
-      res.send('<h1>Timestamp Microservice</h1><br>'+
-      '<h3><a href="http://timestamp-polymorph.herokuapp.com/December%2015,%202015">http://timestamp-polymorph.herokuapp.com/December%2015,%202015</a></h3>'+
-      '<h3><a href="http://timestamp-polymorph.herokuapp.com/1450137600">http://timestamp-polymorph.herokuapp.com/1450137600</a></h3>')
+
+      res.send(obj = {
+        'IP Address': dns.getServers()[0],
+        'Language'  : req.acceptsLanguages()[0],
+        'Software'  : os.platform()
+      })
     })
+// req.get('User-agent');
+          //  var cpu = os.cpus();
+  
+// console.log(os.networkInterfaces()['Wi-Fi'][1].address);
 
-    app.get('/:time', function(req, res) {
+// console.log(http.IncomingMessage);
 
-      var timeVar = req.params.time;
-      var date;
+// dns.resolveSoa('sscript.info', (err, arg) => {
+//   console.log('resolve: ', arg);
+// });
 
-      var formatting = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      };
+//     dns.lookup('nodejs.org', (err, addresses, family) => {
+//   console.log('addresses:', addresses);
+// });
 
-      if (isNaN(timeVar)) {
-
-        date = timeVar;
-        timeVar = new Date(timeVar) / 1000;
-
-        if (isNaN(timeVar)) 
-          date = null;
-
-      } else {
-
-      
-        date = new Date(timeVar * 1000);
-        date = date.toLocaleDateString("en-eu", formatting);
-
-        timeVar = Number(timeVar);
-
-
-
-      }
-
-      res.json({unixtime: timeVar, natural: date})
-
-
-    })
-
-    app.listen(port, function(err, success) {
+    app.listen(3000, function(err, success) {
       if (err) return err;
       console.log("Connected to port 3000");
     });
