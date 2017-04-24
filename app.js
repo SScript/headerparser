c31b848a717a35fce57f424793152408fc46739a
SScript <sscriptnet@gmail.com>
2017-04-24 12:02:42 +0100

    change

--- app.js
+++ app.js
@@ -7,15 +7,23 @@
 
     app.get('/', function(req, res) {
 
-//  var ip = req.headers['x-forwarded-for'] || 
-//      req.connection.remoteAddress || 
-//      req.socket.remoteAddress ||
-//      req.connection.socket.remoteAddress;
+    // var obj = {
+    //     'req.ip': req.ip,
+    //     'x-forwarded-for': req.headers['x-forwarded-for'],
+    //     'x-forwarded-for': req.get('x-forwarded-for'),
+    //     'req.connection.remoteAddress': req.connection.remoteAddress,
+    //     'req.socket.remoteAddress' : req.socket.remoteAddress,
+    //     'req.connection.remotePort': req.connection.remotePort,
+    //     'req.connection.localAddress': req.connection.localAddress,
+    //     'req.connection.localPort': req.connection.localPort,
+    //     'Language'  : req.acceptsLanguages()[0],
+    //     'Software'  : req.get('user-agent')
+    //   }
 
     var obj = {
         'req.ip': req.ip,
-        'x-forwarded-for': req.headers['x-forwarded-for'],
-        'x-forwarded-for': req.get('x-forwarded-for'),
+        'x-forwarded-for1': req.headers['x-forwarded-for'],
+        'x-forwarded-for2': req.get('x-forwarded-for'),
         'req.connection.remoteAddress': req.connection.remoteAddress,
         'req.socket.remoteAddress' : req.socket.remoteAddress,
         'req.connection.remotePort': req.connection.remotePort,
@@ -24,27 +32,11 @@
         'Language'  : req.acceptsLanguages()[0],
         'Software'  : req.get('user-agent')
       }
-//   console.log(req.connection.remoteAddress,
-// req.connection.remotePort,
-// req.connection.localAddress,
-// req.connection.localPort)
-res.json(obj);
-console.log(req.headers['x-forwarded-for'])
-      // console.log(req)
+
+    res.json(obj);
+    console.log(req.headers['x-forwarded-for'])
+
     })
-// req.get('User-agent');
-          //  var cpu = os.cpus();
-//  console.log(os.networkInterfaces()['Wi-Fi'][1].address);
-
-// console.log(http.IncomingMessage);
-
-// dns.resolveSoa('sscript.info', (err, arg) => {
-//   console.log('resolve: ', arg);
-// });
-
-//     dns.lookup('nodejs.org', (err, addresses, family) => {
-//   console.log('addresses:', addresses);
-// });
 
     app.listen(port, function(err, success) {
       if (err) return err;
