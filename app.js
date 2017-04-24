4bb7c6ecabac0ff3c486285cf1016eb55192d560
SScript <sscriptnet@gmail.com>
2017-04-24 10:49:49 +0100

    changed variables

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
     var express = require('express')
     var app = express()
-    var port = Number(process.env.PORT||8080);
+    var port = Number(process.env.PORT||3000);
 
     var dns = require('dns')
     var os = require('os')
@@ -14,17 +14,20 @@
 //      req.connection.socket.remoteAddress;
 
     var obj = {
-        'IP Address': req.ip,
+        'IP Address': req.connection.remoteAddress,
         'Language'  : req.acceptsLanguages()[0],
-        'Software'  : req.headers['user-agent']
+        'Software'  : req.get('user-agent')
       }
+//   console.log(req.connection.remoteAddress,
+// req.connection.remotePort,
+// req.connection.localAddress,
+// req.connection.localPort)
 res.send(obj);
       // console.log(req)
     })
 // req.get('User-agent');
           //  var cpu = os.cpus();
-  
-// console.log(os.networkInterfaces()['Wi-Fi'][1].address);
+//  console.log(os.networkInterfaces()['Wi-Fi'][1].address);
 
 // console.log(http.IncomingMessage);
 
